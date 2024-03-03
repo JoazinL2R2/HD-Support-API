19aca5110fa37b0605e273892f9700ffe1765b39
Joazin <Joazin@DESKTOP-0HK2QBU>
2024-03-03 20:42:23 -0300

    BUGFIX: Relacionamento entre as tabelas e migration

--- HD-Support-API/Data/Map/EmprestimoMap.cs
+++ HD-Support-API/Data/Map/EmprestimoMap.cs
@@ -1,7 +1,6 @@
 ﻿using HD_Support_API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HD_Support_API.Data.Map
 {
@@ -10,11 +9,19 @@ namespace HD_Support_API.Data.Map
         public void Configure(EntityTypeBuilder<Emprestimos> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Funcionario).IsRequired();
-            builder.Property(x => x.FuncionarioId).IsRequired();
-            builder.Property(x => x.Equipamento).IsRequired();
-            builder.Property(x => x.EquipamentoId).IsRequired();
-            builder.Property(x => x.profissional_HD).IsRequired();
+
+            builder.HasOne(x => x.Funcionario)
+                   .WithMany()
+                   .HasForeignKey(x => x.FuncionariosId)
+                   .IsRequired();
+
+            builder.HasOne(x => x.Equipamento)
+                   .WithMany()
+                   .HasForeignKey(x => x.EquipamentosId)
+                   .IsRequired();
+
+            builder.Property(x => x.profissional_HD)
+                   .IsRequired();
         }
     }
 }
--- HD-Support-API/Models/Emprestimos.cs
+++ HD-Support-API/Models/Emprestimos.cs
@@ -3,10 +3,15 @@
     public class Emprestimos
     {
         public int Id { get; set; }
+
+        // Relacionamento com Funcionarios
         public Funcionarios Funcionario { get; set; }
-        public int FuncionarioId { get; set; }
+        public int FuncionariosId { get; set; }
+
+        // Relacionamento com Equipamentos
         public Equipamentos Equipamento { get; set; }
-        public int EquipamentoId { get; set; }
+        public int EquipamentosId { get; set; }
+
         public string? profissional_HD { get; set; }
     }
 }
