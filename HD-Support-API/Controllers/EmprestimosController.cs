32a37b4d7421f832ade7a395a6df5619af70a1d9
waltindobne <145157901+waltindobne@users.noreply.github.com>
2024-03-07 16:36:19 -0300

    Update 0.1.2

--- HD-Support-API/Controllers/EmprestimosController.cs
+++ HD-Support-API/Controllers/EmprestimosController.cs
@@ -11,12 +11,72 @@ namespace HD_Support_API.Controllers
     {
         private readonly IEmprestimoRepositorio _repositorio;
 
-
+        public EmprestimosController(IEmprestimoRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
 
         [HttpGet]
-        public ActionResult<List<Equipamentos>> ListarEmprestimos()
+        [Route("Listar-Emprestimos")]
+        public async Task<IActionResult> ListarEmprestimos()
         {
-            return Ok();
+            var ListarEmprestimos = await _repositorio.ListarEmprestimos();
+            return Ok(ListarEmprestimos);
+        }
+
+        [HttpPost]
+        [Route("Buscar-Emprestimos")]
+        public async Task<IActionResult> AdicionarEmprestimo([FromBody] Emprestimos emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return BadRequest("Dados de Emprestimo não fornecidos");
+            }
+
+            var emprestimoAdicionado = await _repositorio.AdicionarEmprestimo(emprestimo);
+
+            return Ok(emprestimoAdicionado);
+        }
+
+        [HttpPut]
+        [Route("Editar-Emprestimo/{id}")]
+        public async Task<IActionResult> AtualizarEmprestimo(int id, [FromBody] Emprestimos emprestimos)
+        {
+            if (emprestimos == null)
+            {
+                return BadRequest($"Cadastro com ID:{id} não encontrado");
+            }
+
+            var AtualizarEmprestimo = await _repositorio.AtualizarEmprestimo(emprestimos, id);
+            return Ok(AtualizarEmprestimo);
+        }
+
+        [HttpGet]
+        [Route("Buscar-Emprestimo-Por-ID/{id}")]
+        public async Task<IActionResult> BuscarEmprestimosPorID(int id)
+        {
+            var BuscarEmprestimos = await _repositorio.BuscarEmprestimosPorID(id);
+
+            if (BuscarEmprestimos == null)
+            {
+                return NotFound($"Emprestimo com ID:{id} não encontrado");
+            }
+
+            return Ok(BuscarEmprestimos);
+        }
+
+        [HttpPost]
+        [Route("Excluir-Emprestimo/{id}")]
+        public async Task<IActionResult> ExcluirEmprestimo(int id)
+        {
+            var ExcluirEmprestimo = await _repositorio.ExcluirEmprestimo(id);
+
+            if (ExcluirEmprestimo == null)
+            {
+                return NotFound($"Emprestimo com ID:{id} não encontrado");
+            }
+
+            return Ok(ExcluirEmprestimo);
         }
     }
 }
--- HD-Support-API/Controllers/EquipamentosController.cs
+++ HD-Support-API/Controllers/EquipamentosController.cs
@@ -1,4 +1,5 @@
 ﻿using HD_Support_API.Models;
+using HD_Support_API.Repositorios.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,74 @@ namespace HD_Support_API.Controllers
     [ApiController]
     public class EquipamentosController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<Equipamentos>> ListarTodosEquipamentos()
+        private readonly IEquipamentosRepositorio _repositorio;
+
+        public EquipamentosController(IEquipamentosRepositorio repositorio)
         {
-            return Ok();
+            _repositorio = repositorio;
+        }
+
+        [HttpGet]
+        [Route("Listar-Equipamentos")]
+        public async Task<IActionResult> ListarEquipamentos()
+        {
+            var ListarEquipamentos = await _repositorio.ListarEquipamentos();
+            return Ok(ListarEquipamentos);
+        }
+
+        [HttpPost]
+        [Route("Buscar-Equipamentos")]
+        public async Task<IActionResult> AdicionarEquipamento([FromBody] Equipamentos equipamentos)
+        {
+            if (equipamentos == null)
+            {
+                return BadRequest("Dados do Equipamento não fornecidos");
+            }
+
+            var equipamentoAdicionado = await _repositorio.AdicionarEquipamento(equipamentos);
+
+            return Ok(equipamentoAdicionado);
+        }
+
+        [HttpPut]
+        [Route("Editar-Equipamento/{id}")]
+        public async Task<IActionResult> AtualizarEquipamento(int id, [FromBody] Equipamentos equipamentos)
+        {
+            if (equipamentos == null)
+            {
+                return BadRequest($"Equipamento com ID:{id} não encontrado");
+            }
+
+            var AtualizarEquipamento = await _repositorio.AtualizarEquipamento(equipamentos, id);
+            return Ok(AtualizarEquipamento);
+        }
+
+        [HttpGet]
+        [Route("Buscar-Equipamento-Por-ID/{id}")]
+        public async Task<IActionResult> BuscarEquipamentos(int id)
+        {
+            var buscarEquipamentos = await _repositorio.BuscarEquipamentos(id);
+
+            if (buscarEquipamentos == null)
+            {
+                return NotFound($"Equipamento com ID:{id} não encontrado");
+            }
+
+            return Ok(buscarEquipamentos);
+        }
+
+        [HttpPost]
+        [Route("Excluir-Equipamento/{id}")]
+        public async Task<IActionResult> ExcluirEquipamento(int id)
+        {
+            var excluirEmprestimo = await _repositorio.ExcluirEquipamento(id);
+
+            if (excluirEmprestimo == null)
+            {
+                return NotFound($"Equipamento com ID:{id} não encontrado");
+            }
+
+            return Ok(excluirEmprestimo);
         }
     }
 }
